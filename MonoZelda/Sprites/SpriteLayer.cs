a62a511ac7ec6a2505aeaabe73d7ef5fd8a0a112
Josh <slaven.18@osu.edu>
2024-10-12 11:02:44 -0400

    Refactored dungeon loader to make it more obvious what was going on.

---
+++ MonoZelda/Sprites/SpriteLayer.cs
@@ -0,0 +1,11 @@
+﻿namespace MonoZelda.Sprites;
+
+internal static class SpriteLayer
+{
+    public static readonly int Background = -10;
+    public static readonly int Blocks = 0;
+    public static readonly int Items = 50;
+    public static readonly int Enemies = 10;
+    public static readonly int Player = 15;
+    public static readonly int Projectiles = 20;
+}
