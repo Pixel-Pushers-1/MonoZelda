bd3f0baa09e2101f2243f1855296ae39b08580ec
Josh <slaven.18@osu.edu>
2024-10-12 11:02:44 -0400

    Refactored dungeon loader to make it more obvious what was going on.

--- MonoZelda/Scenes/ExperimentalDungeonLoader.cs
+++ MonoZelda/Scenes/ExperimentalDungeonLoader.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using MonoZelda.Collision;
 using MonoZelda.Dungeons;
+using MonoZelda.Sprites;
 using PixelPushers.MonoZelda.Sprites;
 using PixelPushers.MonoZelda.Tiles;
 
@@ -20,142 +21,117 @@ public class ExperimentalDungeonLoader
 
     private static string Dungeon1 = "https://docs.google.com/spreadsheets/d/1LJPdekWHcv_nLglTE_mb_izUfJeQiEXoGHPhfGcHD-M/gviz/tq?tqx=out:csv&sheet=Room1";
 
+    private const int TileWidth = 64;
+    private const int TileHeight = 64;
+    private static readonly Point Margin = new Point(64, 64);
+    private static readonly Point DungeonPoint = new Point(0, 192);
+
     public ExperimentalDungeonLoader(ContentManager contentManager, CollidablesManager cm, GraphicsDevice graphicsDevice)
     {
-        // Load the perimeter
-        var dungonPoint = new Point(0, 192);
+        var texture = contentManager.Load<Texture2D>(TextureData.Blocks);
+        LoadBorder(texture);
+        LoadDungeon(contentManager, cm, graphicsDevice, texture);
+    }
+
+    private void LoadBorder(Texture2D texture)
+    {
         var roomPoint = new Point(128, 128);
-        var northDoorPoint = new Point(448, 0);
-        var eastDoorPoint = new Point(896, 288);
-        var southDoorPoint = new Point(448, 576);
-        var westDoorPoint = new Point(0, 288);
-
-        var d = new SpriteDict(contentManager.Load<Texture2D>(TextureData.Blocks), SpriteCSVData.Blocks, -10, dungonPoint);
+        var d = new SpriteDict(texture, SpriteCSVData.Blocks, SpriteLayer.Background, DungeonPoint);
         d.SetSprite("room_exterior");
+    }
 
-        // Load the dungeon from the URI
-        using var dungeon1Stream = DownloadCsvStream(Dungeon1);
+    private void LoadDungeon(ContentManager contentManager, CollidablesManager cm, GraphicsDevice graphicsDevice, Texture2D texture)
+    {
+        using var dungeon1Stream = DownloadCsvStreamAsync(Dungeon1).Result;
 
         if (dungeon1Stream != null)
         {
             using var streamReader = new StreamReader(dungeon1Stream);
-
-            // Set up text parser
             using TextFieldParser textFieldParser = new TextFieldParser(streamReader);
             textFieldParser.TextFieldType = FieldType.Delimited;
             textFieldParser.SetDelimiters(",");
 
-            // Nothing to do if the file is empty
-            if (textFieldParser.EndOfData)
-            {
-                return;
-            }
+            if (textFieldParser.EndOfData) return;
 
-            // First row describes the room bg
             string[] roomDef = textFieldParser.ReadFields();
-            // roomDef[0] should be a Dungeon1 sprite
-            if (Enum.TryParse(roomDef[0], out Dungeon1Sprite roomSprite))
+            LoadRoomBackground(contentManager, DungeonPoint, roomDef);
+            LoadDoors(texture, DungeonPoint, roomDef);
+            LoadCollisions(textFieldParser, cm, graphicsDevice, DungeonPoint);
+        }
+
+        // TODO: Indicate error downloading CSV
+    }
+
+    private void LoadRoomBackground(ContentManager contentManager, Point dungeonPoint, string[] roomDef)
+    {
+        var roomPoint = new Point(128, 128);
+        if (Enum.TryParse(roomDef[0], out Dungeon1Sprite roomSprite))
+        {
+            var r = new SpriteDict(contentManager.Load<Texture2D>(TextureData.Blocks), SpriteCSVData.Blocks, SpriteLayer.Background, dungeonPoint + roomPoint);
+            r.SetSprite(roomSprite.ToString());
+        }
+    }
+
+    private void LoadDoors(Texture2D texture, Point dungeonPoint, string[] roomDef)
+    {
+        var doorPoints = new Point[]
+        {
+            new (448, 0),
+            new (896, 288),
+            new (448, 576),
+            new (0, 288)
+        };
+
+        for (int i = 1; i <= 4; i++)
+        {
+            if (Enum.TryParse(roomDef[i], out Dungeon1Sprite doorSprite))
             {
-                var r = new SpriteDict(contentManager.Load<Texture2D>(TextureData.Blocks), SpriteCSVData.Blocks, 0, dungonPoint + roomPoint);
-                r.SetSprite(roomSprite.ToString());
-            }
-
-            // Cells 1-4 describe the doors
-            SpriteDict doorDict = null;
-            if (Enum.TryParse(roomDef[1], out Dungeon1Sprite northDoorSprite))
-            {
-
-                doorDict = new SpriteDict(contentManager.Load<Texture2D>(TextureData.Blocks), SpriteCSVData.Blocks, 1, dungonPoint + northDoorPoint);
-                doorDict.SetSprite(northDoorSprite.ToString());
-            }
-            if (Enum.TryParse(roomDef[2], out Dungeon1Sprite eastDoorSprite))
-            {
-                doorDict = new SpriteDict(contentManager.Load<Texture2D>(TextureData.Blocks), SpriteCSVData.Blocks, 1, dungonPoint + eastDoorPoint);
-                doorDict.SetSprite(eastDoorSprite.ToString());
-            }
-            if (Enum.TryParse(roomDef[3], out Dungeon1Sprite southDoorSprite))
-            {
-                doorDict = new SpriteDict(contentManager.Load<Texture2D>(TextureData.Blocks), SpriteCSVData.Blocks, 1, dungonPoint + southDoorPoint);
-                doorDict.SetSprite(southDoorSprite.ToString());
-            }
-            if (Enum.TryParse(roomDef[4], out Dungeon1Sprite westDoorSprite))
-            {
-                doorDict = new SpriteDict(contentManager.Load<Texture2D>(TextureData.Blocks), SpriteCSVData.Blocks, 1, dungonPoint + westDoorPoint);
-                doorDict.SetSprite(westDoorSprite.ToString());
-            }
-
-            // Loop through CSV file
-            var i = 0;
-            var tileWidth = 64;
-            var tileHeight = 64;
-            var margin = new Point(64,64);
-            while (!textFieldParser.EndOfData)
-            {
-                string[] fields = textFieldParser.ReadFields();
-                var j = 0;
-
-                foreach (var field in fields)
-                {
-                    // Try and parse the field as a BlockType enum
-                    if (Enum.TryParse(field, out CollisionTileRect collisionRect))
-                    {
-                        var position = new Point(j * tileWidth, i * tileHeight) + dungonPoint + margin;
-
-                        var rectSize = new Point(tileWidth, tileHeight);
-                        Rectangle rect;
-
-                        switch (collisionRect)
-                        {
-                            case CollisionTileRect.collision_top:
-                                rectSize = new Point(tileWidth, tileHeight / 2);
-                                rect = new Rectangle(position, rectSize);
-                                break;
-
-                            case CollisionTileRect.collision_bottom:
-                                rectSize = new Point(tileWidth, tileHeight / 2);
-                                rect = new Rectangle(new Point(position.X, position.Y + tileHeight / 2), rectSize);
-                                break;
-
-                            case CollisionTileRect.collision_left:
-                                rectSize = new Point(tileWidth / 2, tileHeight);
-                                rect = new Rectangle(position, rectSize);
-                                break;
-
-                            case CollisionTileRect.collision_right:
-                                rectSize = new Point(tileWidth / 2, tileHeight);
-                                rect = new Rectangle(new Point(position.X + tileWidth / 2, position.Y), rectSize);
-                                break;
-
-                            case CollisionTileRect.collision_full:
-                            default:
-                                rect = new Rectangle(position, new Point(tileWidth, tileHeight));
-                                break;
-                        }
-
-                        Collidable itemHitbox = new Collidable(rect, graphicsDevice);
-                        cm.AddHitbox(itemHitbox);
-                    }
-
-                    j++;
-                }
-
-                i++;
+                var doorDict = new SpriteDict(texture, SpriteCSVData.Blocks, SpriteLayer.Blocks, dungeonPoint + doorPoints[i - 1]);
+                doorDict.SetSprite(doorSprite.ToString());
             }
         }
     }
 
-    public Stream DownloadCsvStream(string url)
+    private void LoadCollisions(TextFieldParser textFieldParser, CollidablesManager cm, GraphicsDevice graphicsDevice, Point dungeonPoint)
+    {
+        var i = 0;
+
+        while (!textFieldParser.EndOfData)
+        {
+            string[] fields = textFieldParser.ReadFields();
+            for (int j = 0; j < fields.Length; j++)
+            {
+                if (Enum.TryParse(fields[j], out CollisionTileRect collisionRect))
+                {
+                    var position = new Point(j * TileWidth, i * TileHeight) + dungeonPoint + Margin;
+                    var rect = GetCollisionRectangle(collisionRect, position, TileWidth, TileHeight);
+                    var itemHitbox = new Collidable(rect, graphicsDevice);
+                    cm.AddHitbox(itemHitbox);
+                }
+            }
+            i++;
+        }
+    }
+
+    private Rectangle GetCollisionRectangle(CollisionTileRect collisionRect, Point position, int tileWidth, int tileHeight)
+    {
+        return collisionRect switch
+        {
+            CollisionTileRect.collision_top => new Rectangle(position, new Point(tileWidth, tileHeight / 2)),
+            CollisionTileRect.collision_bottom => new Rectangle(new Point(position.X, position.Y + tileHeight / 2), new Point(tileWidth, tileHeight / 2)),
+            CollisionTileRect.collision_left => new Rectangle(position, new Point(tileWidth / 2, tileHeight)),
+            CollisionTileRect.collision_right => new Rectangle(new Point(position.X + tileWidth / 2, position.Y), new Point(tileWidth / 2, tileHeight)),
+            CollisionTileRect.collision_full or _ => new Rectangle(position, new Point(tileWidth, tileHeight)),
+        };
+    }
+
+    public async Task<Stream> DownloadCsvStreamAsync(string url)
     {
         try
         {
-            // Send a synchronous GET request to the specified URL
-            var response = httpClient.GetAsync(url).Result;
-            
-            // Ensure that the request was successful
+            var response = await httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
-
-            // Return the response content as a stream (CSV content)
-            return response.Content.ReadAsStreamAsync().Result;
+            return await response.Content.ReadAsStreamAsync();
         }
         catch (Exception ex)
         {
