13e9e5926890387e3eccf43eb088a12955b89ec3
joshua-klasmeier <klasmeier.1@buckeyemail.osu.edu>
2024-10-20 15:11:42 -0400

    Spawn Link in Correct Position, Player Static Block Collision (#66)
    * Spawn Link in Correct Position, Player Static Block Collision
    
    * Run scene update before the collsion controller so the colliders are done moving around before we check for overlaps.
    
    ---------
    
    Co-authored-by: xrs6969 <118093553+xrs6969@users.noreply.github.com>
    Co-authored-by: Josh <slaven.18@osu.edu>

--- MonoZelda/Commands/CollisionCommands/PlayerStaticCollisionCommand.cs
+++ MonoZelda/Commands/CollisionCommands/PlayerStaticCollisionCommand.cs
@@ -1,22 +1,52 @@
-﻿namespace MonoZelda.Commands.CollisionCommands;
+﻿using Microsoft.Xna.Framework;
+using MonoZelda.Collision;
+using MonoZelda.Controllers;
+using MonoZelda.Link;
+using System.Diagnostics;
+
+namespace MonoZelda.Commands.CollisionCommands;
 
 public class PlayerStaticCollisionCommand : ICommand
 {
     private MonoZeldaGame game;
-
+    private Player player;
     public PlayerStaticCollisionCommand()
     {
         //empty
     }
 
-    public PlayerStaticCollisionCommand(MonoZeldaGame game)
+    public PlayerStaticCollisionCommand(Player player)
     {
-        this.game = game;
+        this.player = player;
     }
 
     public void Execute(params object[] metadata)
     {
-        // ADD EXECUTE LOGIC HERE
+        Collidable collidableA = (Collidable)metadata[0];
+        Collidable collidableB = (Collidable)metadata[1];
+        CollisionController collisionController = (CollisionController)metadata[2];
+        Direction collisionDirection = (Direction)metadata[3];
+        Rectangle intersection = (Rectangle)metadata[4];
+
+        Vector2 currentPos = player.GetPlayerPosition();
+
+        switch (collisionDirection)
+        {
+            case Direction.Left:
+                currentPos.X -= intersection.Width;
+                break;
+            case Direction.Right:
+                currentPos.X += intersection.Width;
+                break;
+            case Direction.Up:
+                currentPos.Y -= intersection.Height;
+                break;
+            case Direction.Down:
+                currentPos.Y += intersection.Height;
+                break;
+        }
+
+        player.SetPosition(currentPos);
     }
 
     public void UnExecute()
