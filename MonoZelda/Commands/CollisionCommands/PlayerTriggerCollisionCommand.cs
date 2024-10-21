c9d76bd8bd282113bb6016d4047ac2fe1827aa2e
Josh <slaven.18@osu.edu>
2024-10-21 10:45:59 -0400

    New type of collision called a Trigger PushBlockTrigger uses this to implment the push block.

---
+++ MonoZelda/Commands/CollisionCommands/PlayerTriggerCollisionCommand.cs
@@ -0,0 +1,41 @@
+﻿using MonoZelda.Trigger;
+using MonoZelda.Link;
+using System;
+using System.Collections.Generic;
+
+namespace MonoZelda.Commands.CollisionCommands
+{
+    public class PlayerTriggerCollisionCommand : ICommand
+    {
+
+        public PlayerTriggerCollisionCommand()
+        {
+            //empty
+        }
+
+        public PlayerTriggerCollisionCommand(ITrigger trigger)
+        {
+        }
+
+        public void Execute(params object[] metadata)
+        {
+            if (metadata[3] is Direction direction)
+            {
+                if (metadata[0] is ITrigger t1)
+                {
+                    t1.Trigger(direction);
+                }
+                if (metadata[2] is ITrigger t2)
+                {
+                    t2.Trigger(direction);
+                }
+            }
+
+        }
+
+        public void UnExecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
