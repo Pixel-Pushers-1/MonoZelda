0a446dd02bee37ddc7c138251fe74d1732c59955
xrs6969 <118093553+xrs6969@users.noreply.github.com>
2024-10-22 18:59:51 -0400

    keyboard controller refeactor

--- MonoZelda/Controllers/KeyboardController.cs
+++ MonoZelda/Controllers/KeyboardController.cs
@@ -3,101 +3,112 @@ using Microsoft.Xna.Framework.Input;
 using MonoZelda.Commands;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
+
 namespace MonoZelda.Controllers;
 
 public class KeyboardController : IController
 {
-    private readonly CommandManager _commandManager;
-    private readonly Dictionary<Keys, (CommandType command, bool oneShot)> _keyBindings;
-    private readonly Dictionary<Keys, double> _keyPressTimestamps;
-    private double _currentTimestamp;
+    private CommandManager _commandManager;
+    private Dictionary<(Keys key, bool oneShot), CommandType> _keyCommandDictionary;
+    private List<Keys> _pressedDirectionalKeys;
+    private static readonly Keys[] _directionalKeys = new[]
+    {
+        Keys.W, Keys.Up,
+        Keys.S, Keys.Down,
+        Keys.A, Keys.Left,
+        Keys.D, Keys.Right
+    };
 
     public KeyboardController(CommandManager commandManager)
     {
         _commandManager = commandManager;
-        _keyBindings = new Dictionary<Keys, (CommandType, bool)>();
-        _keyPressTimestamps = new Dictionary<Keys, double>();
-        _currentTimestamp = 0;
-        // boolean passed in if is one shot 
-        AddBinding(Keys.Enter, CommandType.StartGameCommand, true);
-        AddBinding(Keys.W, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.Up, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.S, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.Down, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.D, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.Right, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.A, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.Left, CommandType.PlayerMoveCommand, false);
-        AddBinding(Keys.G, CommandType.ToggleGizmosCommand, true);
-        AddBinding(Keys.D1, CommandType.PlayerUseItemCommand, true);
-        AddBinding(Keys.D2, CommandType.PlayerUseItemCommand, true);
-        AddBinding(Keys.D3, CommandType.PlayerUseItemCommand, true);
-        AddBinding(Keys.D4, CommandType.PlayerUseItemCommand, true);
-        AddBinding(Keys.D5, CommandType.PlayerUseItemCommand, true);
-        AddBinding(Keys.D6, CommandType.PlayerUseItemCommand, true);
-        AddBinding(Keys.T, CommandType.PlayerFireSwordBeam, true);
-        AddBinding(Keys.Z, CommandType.PlayerAttackCommand, true);
-        AddBinding(Keys.N, CommandType.PlayerAttackCommand, true);
-        AddBinding(Keys.Q, CommandType.ExitCommand, true);
-        AddBinding(Keys.R, CommandType.ResetCommand, true);
-        AddBinding(Keys.None, CommandType.PlayerStandingCommand, false);
+        _pressedDirectionalKeys = new List<Keys>();
+        _keyCommandDictionary = new Dictionary<(Keys key, bool oneShot), CommandType>
+        {
+            {new (Keys.Enter, true), CommandType.StartGameCommand},
+            {new (Keys.W, false), CommandType.PlayerMoveCommand},
+            {new (Keys.Up, false), CommandType.PlayerMoveCommand},
+            {new (Keys.S, false), CommandType.PlayerMoveCommand},
+            {new (Keys.Down, false), CommandType.PlayerMoveCommand},
+            {new (Keys.D, false), CommandType.PlayerMoveCommand},
+            {new (Keys.Right, false), CommandType.PlayerMoveCommand},
+            {new (Keys.A, false), CommandType.PlayerMoveCommand},
+            {new (Keys.Left, false), CommandType.PlayerMoveCommand},
+            {new (Keys.G, true), CommandType.ToggleGizmosCommand},
+            {new (Keys.D1, true), CommandType.PlayerUseItemCommand},
+            {new (Keys.D2, true), CommandType.PlayerUseItemCommand},
+            {new (Keys.D3, true), CommandType.PlayerUseItemCommand},
+            {new (Keys.D4, true), CommandType.PlayerUseItemCommand},
+            {new (Keys.D5, true), CommandType.PlayerUseItemCommand},
+            {new (Keys.D6, true), CommandType.PlayerUseItemCommand},
+            {new (Keys.T, true), CommandType.PlayerFireSwordBeam},
+            {new (Keys.Z, true), CommandType.PlayerAttackCommand},
+            {new (Keys.N, true), CommandType.PlayerAttackCommand},
+            {new (Keys.Q, false), CommandType.ExitCommand},
+            {new (Keys.R, false), CommandType.ResetCommand},
+            {new (Keys.None, false), CommandType.PlayerStandingCommand},
+        };
     }
 
     public KeyboardState CurrentKeyboardState { get; private set; }
     public KeyboardState PreviousKeyboardState { get; private set; }
 
-    private void AddBinding(Keys key, CommandType command, bool oneShot)
-    {
-        _keyBindings[key] = (command, oneShot);
-    }
-
     public void Update(GameTime gameTime)
     {
-        _currentTimestamp += gameTime.ElapsedGameTime.TotalMilliseconds;
         CurrentKeyboardState = Keyboard.GetState();
+        UpdateDirectionalKeysList();
 
-        foreach (var key in _keyBindings.Keys)
+        if (_pressedDirectionalKeys.Any())
         {
-            if (key != Keys.None &&
-                CurrentKeyboardState.IsKeyDown(key) &&
-                (!_keyPressTimestamps.ContainsKey(key) || !PreviousKeyboardState.IsKeyDown(key)))
-            {
-                _keyPressTimestamps[key] = _currentTimestamp;
-            }
-        }
-
-        foreach (var key in _keyPressTimestamps.Keys.ToList())
-        {
-            if (CurrentKeyboardState.IsKeyUp(key))
-            {
-                _keyPressTimestamps.Remove(key);
-            }
-        }
-
-        Keys activeKey = Keys.None;
-
-        if (_keyPressTimestamps.Any())
-        {
-            activeKey = _keyPressTimestamps
-                .OrderByDescending(kvp => kvp.Value)
-                .First()
-                .Key;
-
-            var (command, oneShot) = _keyBindings[activeKey];
-
-            if (!oneShot || OneShotPressed(activeKey))
-            {
-                _commandManager.Execute(command, activeKey);
-            }
+            Keys mostRecentKey = _pressedDirectionalKeys.Last();
+            _commandManager.Execute(CommandType.PlayerMoveCommand, mostRecentKey);
         }
         else
         {
             _commandManager.Execute(CommandType.PlayerStandingCommand, Keys.None);
         }
 
+        foreach (var keyCommandPair in _keyCommandDictionary)
+        {
+            (Keys key, bool oneShot) = keyCommandPair.Key;
+
+            if (_directionalKeys.Contains(key))
+                continue;
+
+            if (!oneShot && CurrentKeyboardState.IsKeyDown(key))
+            {
+                _commandManager.Execute(keyCommandPair.Value, key);
+                break;
+            }
+            else if (oneShot && OneShotPressed(key))
+            {
+                _commandManager.Execute(keyCommandPair.Value, key);
+                break;
+            }
+        }
+
         PreviousKeyboardState = CurrentKeyboardState;
     }
 
+    private void UpdateDirectionalKeysList()
+    {
+        foreach (Keys key in _directionalKeys)
+        {
+            bool isCurrentlyPressed = CurrentKeyboardState.IsKeyDown(key);
+            bool wasPressed = PreviousKeyboardState.IsKeyDown(key);
+
+            if (isCurrentlyPressed && !wasPressed)
+            {
+                _pressedDirectionalKeys.Remove(key);
+                _pressedDirectionalKeys.Add(key);
+            }
+            else if (!isCurrentlyPressed && wasPressed)
+            {
+                _pressedDirectionalKeys.Remove(key);
+            }
+        }
+    }
     private bool OneShotPressed(Keys key)
     {
         return CurrentKeyboardState.IsKeyDown(key) && !PreviousKeyboardState.IsKeyDown(key);
