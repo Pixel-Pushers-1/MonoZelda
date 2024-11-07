f5cba90735f970b8d32b4273549549d0c41151ea
Om Kurkure <kurkureom@gmail.com>
2024-11-06 23:13:14 -0500

    Adding Mute functionality and refactoring how sounds are played

--- MonoZelda/Sound/SoundManager.cs
+++ MonoZelda/Sound/SoundManager.cs
@@ -6,43 +6,73 @@ using System.Collections.Generic;
 namespace MonoZelda.Sound;
 
 public static class SoundManager
-{    
+{
     private static ContentManager contentManager;
-    private static Dictionary<string, SoundEffectInstance> loopedSoundEffects;
+    private static Dictionary<string, SoundEffectInstance> soundEffects;
+    private static bool Muted;
 
     // Static initialization method to set up ContentManager and other resources
     public static void Initialize(ContentManager content)
     {
         contentManager = content;
-        loopedSoundEffects = new Dictionary<string, SoundEffectInstance>();
+        soundEffects = new Dictionary<string, SoundEffectInstance>();
+        Muted = false;  
     }
 
     public static void PlaySound(string soundName, bool Looped)
     {
-        if (!loopedSoundEffects.ContainsKey(soundName))
+        if (!soundEffects.ContainsKey(soundName))
         {
-            //load sound
+            // load sound
             string soundFilePath = "Sound/" + soundName;
             SoundEffect soundEffect = contentManager.Load<SoundEffect>(soundFilePath);
             SoundEffectInstance soundInstance = soundEffect.CreateInstance();
-
             soundInstance.IsLooped = Looped;
-            if (Looped == true)
-            {
-                loopedSoundEffects.Add(soundName, soundInstance);
-            }
+
+            // Add sound instance to the dictionary
+            soundEffects.Add(soundName, soundInstance);
 
             // play sound
             soundInstance.Play();
+        }
+        else
+        {
+            soundEffects[soundName].Play();
         }
     }
 
     public static void StopSound(string soundName)
     {
-        if (loopedSoundEffects.ContainsKey(soundName))
+        if (soundEffects.ContainsKey(soundName))
         {
-            SoundEffectInstance soundInstance = loopedSoundEffects[soundName];
+            SoundEffectInstance soundInstance = soundEffects[soundName];
             soundInstance.Stop();
+            soundEffects.Remove(soundName);
         }
+    }
+
+    public static void ChangeMuteState()
+    {
+        if (Muted == false)
+        {
+            foreach (SoundEffectInstance soundEffect in soundEffects.Values)
+            {
+                soundEffect.Volume = 0;
+            }
+            Muted = true;
+        }
+        else
+        {
+            foreach (SoundEffectInstance soundEffect in soundEffects.Values)
+            {
+                soundEffect.Volume = 1;
+            }
+            Muted = false;
+        }
+    }
+
+    public static void ClearSoundDictionary()
+    {
+        soundEffects.Clear();
     }
 }
