2596ecc52190d2f09adccb4bdb4c0d440a410804
xrs6969 <klasmeier.1@buckeyemail.osu.edu>
2024-11-11 11:49:48 -0500

    fix merge

--- MonoZelda/Enemies/EnemyClasses/Oldman.cs
+++ MonoZelda/Enemies/EnemyClasses/Oldman.cs
@@ -18,35 +18,15 @@ namespace MonoZelda.Enemies.EnemyClasses
 
         public override void EnemySpawn(SpriteDict enemyDict, Point spawnPosition, CollisionController collisionController)
         {
-            //adjusted position to spawn in middle of screen since we cant with current implementation
-            Point adjustedPosition = new Point(spawnPosition.X - 32, spawnPosition.Y + 64);
-
-            EnemyHitbox = new EnemyCollidable(new Rectangle(adjustedPosition.X, adjustedPosition.Y, Width, Height), EnemyList.Oldman);
-            collisionController.AddCollidable(EnemyHitbox);
-            EnemyHitbox.setSpriteDict(enemyDict);
-            enemyDict.Position = adjustedPosition;
-            EnemyHitbox.UnregisterHitbox();
-            collisionController.RemoveCollidable(EnemyHitbox);
-            // Immediately set to oldman sprite instead of cloud
-            enemyDict.SetSprite("oldman");
-            oldmanSpriteDict = enemyDict;
-            Pos = adjustedPosition;
+            Pos = spawnPosition;
+            StateMachine = new EnemyStateMachine(enemyDict);
+            StateMachine.SetSprite("oldman");
+            PixelsMoved = 0;
         }
 
-        public void ChangeDirection()
+        public override void Update()
         {
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            if (spawnTimer >= 64)
-            {
-                oldmanSpriteDict.SetSprite("oldman");
-            }
-            else
-            {
-                spawnTimer++;
-            }
+            StateMachine.Update(this, Pos);
         }
 
         public override void TakeDamage(float stunTime, Direction collisionDirection, int damage)
