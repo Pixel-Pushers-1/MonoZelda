ac26919aabb3ceafd01fabc509c63a4a47024895
xsedim <127362018+xsedim@users.noreply.github.com>
2024-11-12 14:36:26 -0500

    Carson flashing sprites (#110)
    * Added flashing effects
    Also removed GameTime references being passed around in the sprite drawing system since that's a global variable now.
    
    * Added flashing effects
    Also removed GameTime references being passed around in the sprite drawing system since that's a global variable now.
    
    * Integrated flashing damage animations
    Player and enemies flash when damaged, items flash when spawning.
    Player health resets on game reset.
    
    * Added BlankSprites
    Updated EnterDungeonScene curtains to use BlankSprites
    
    * Added bomb explosion animations
    
    * Added flashing effects
    Also removed GameTime references being passed around in the sprite drawing system since that's a global variable now.
    
    * Integrated flashing damage animations
    Player and enemies flash when damaged, items flash when spawning.
    Player health resets on game reset.
    
    * Added BlankSprites
    Updated EnterDungeonScene curtains to use BlankSprites
    
    * Added bomb explosion animations
    
    * Rebase fixes
    
    * Removed old code

--- MonoZelda/Items/ItemFactory.cs
+++ MonoZelda/Items/ItemFactory.cs
@@ -9,7 +9,9 @@ using System;
 namespace MonoZelda.Items;
 
 public class ItemFactory
-{ 
+{
+    private const float FLASHING_TIME = .75f;
+
     private CollisionController collisionController;
 
     public ItemFactory( CollisionController collisionController)
@@ -20,6 +22,7 @@ public class ItemFactory
     public void CreateItem(ItemList itemName, Point spawnPosition)
     {
         var itemDict = new SpriteDict(SpriteType.Items, 0, new Point(0, 0));
+        itemDict.SetFlashing(SpriteDict.FlashingType.OnOff, FLASHING_TIME);
         var itemType = Type.GetType($"MonoZelda.Items.ItemClasses.{itemName}");
         IItem item = (IItem)Activator.CreateInstance(itemType);
         item.itemSpawn(itemDict, spawnPosition, collisionController);
