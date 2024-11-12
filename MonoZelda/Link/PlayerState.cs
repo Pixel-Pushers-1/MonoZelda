3dc9a6458e5a4700002765470f3cde3fef89511b
Carson Miller <xsedim@gmail.com>
2024-11-12 01:58:08 -0500

    Added bomb explosion animations

--- MonoZelda/Link/PlayerState.cs
+++ MonoZelda/Link/PlayerState.cs
@@ -7,7 +7,11 @@ namespace MonoZelda.Link;
 
 public static class PlayerState
 {
-    private static int INITIAL_HP =6;
+    private static readonly int INITIAL_HP = 6;
+    private static readonly int INITIAL_RUPEES = 3;
+    private static readonly int INITIAL_BOMBS = 1;
+    private static readonly int INITIAL_KEYS = 0;
+
     private static int _health = INITIAL_HP;
 
     public static void Initialize()
@@ -19,9 +23,9 @@ public static class PlayerState
         IsKnockedBack = false;
         _health = INITIAL_HP;
         MaxHealth = INITIAL_HP;
-        Rupees = 0;
-        Bombs = 0;
-        Keys = 0;
+        Rupees = INITIAL_RUPEES;
+        Bombs = INITIAL_BOMBS;
+        Keys = INITIAL_KEYS;
         EquippedProjectile = ProjectileType.None;
 
 
