48eb28a511842930ee2fb5b2f94544c097426cfe
Om Kurkure <kurkureom@gmail.com>
2024-11-30 02:31:36 -0500

    adding wallMasterAnimation

--- MonoZelda/Scenes/WallMasterGrabScene.cs
+++ MonoZelda/Scenes/WallMasterGrabScene.cs
@@ -6,24 +6,24 @@ using MonoZelda.Sprites;
 using MonoZelda.Dungeons;
 using System.Collections.Generic;
 using MonoZelda.Commands;
-using MonoZelda.Sound;
 
 namespace MonoZelda.Scenes;
 
 public class WallMasterGrabScene : Scene
 {
-    // movement zones
-    private static readonly Rectangle MoveLeftZoneTop = new Rectangle(new Point(192, 320), new Point(704, 64));
-    private static readonly Rectangle MoveLeftZoneBottom = new Rectangle(new Point(192, 704), new Point(704, 64));
-    private static readonly Rectangle MoveDownDoorZone = new Rectangle(new Point(128, 320), new Point(64, 192));
-    private static readonly Rectangle MoveUpDoorZone = new Rectangle(new Point(128, 576), new Point(64, 192));
-    private static readonly Rectangle MoveUpZone = new Rectangle(new Point(832, 384), new Point(64, 160));
-    private static readonly Rectangle MoveDownZone = new Rectangle(new Point(832, 544), new Point(64, 160));
-    private static readonly Rectangle StopZone = new Rectangle(new Point(128, 512), new Point(64, 64));
+    // movement zones; Integer represent priority of zones in case player is in two movement zones
+    private static readonly (Rectangle, int) MoveUpZone = (new Rectangle(new Point(832, 384), new Point(64, 160)), 1);
+    private static readonly (Rectangle, int) MoveDownZone = (new Rectangle(new Point(832, 544), new Point(64, 160)), 1);
+    private static readonly (Rectangle,int) MoveLeftZoneTop = (new Rectangle(new Point(192, 320), new Point(704, 64)),2);
+    private static readonly (Rectangle,int) MoveLeftZoneBottom = (new Rectangle(new Point(192, 704), new Point(704, 64)), 2);
+    private static readonly (Rectangle, int) MoveDownDoorZone = (new Rectangle(new Point(128, 320), new Point(64, 192)), 3);
+    private static readonly (Rectangle, int) MoveUpDoorZone = (new Rectangle(new Point(128, 576), new Point(64, 192)), 3);
+    private static readonly (Rectangle, int) StopZone = (new Rectangle(new Point(128, 512), new Point(64, 64)), 4);
 
     // variables
     private bool reachedDoor;
     private string currentRoomSprite;
+    private int MaxIntersectionArea;
     private ICommand loadRoomCommand;
     private Direction movementDirection;
     private IDungeonRoom startRoom;
@@ -31,19 +31,23 @@ public class WallMasterGrabScene : Scene
     private SpriteDict FakeLink;
     private SpriteDict FakeWallMaster;
     private SpriteDict FakeBackground;
+    private SpriteDict FakeBorder;
     private BlankSprite LeftCurtain;
     private BlankSprite RightCurtain;
     private GraphicsDevice graphicsDevice;
 
+    // Add a Texture2D for drawing rectangles
+    private Texture2D rectangleTexture;
+
     // Direction Map
-    private static readonly Dictionary<Rectangle, Direction> DirectionMap = new()
+    private static readonly Dictionary<(Rectangle,int), Direction> DirectionMap = new()
     {
-        {MoveDownDoorZone,Direction.Down},
-        {MoveUpDoorZone,Direction.Up},
-        {MoveLeftZoneBottom,Direction.Left},
-        {MoveLeftZoneTop,Direction.Left},
-        {MoveDownZone,Direction.Down},
-        {MoveUpZone,Direction.Up},
+        {MoveDownDoorZone,Direction.Up},
+        {MoveUpDoorZone,Direction.Down},
+        {MoveLeftZoneBottom,Direction.Right},
+        {MoveLeftZoneTop,Direction.Right},
+        {MoveDownZone,Direction.Up},
+        {MoveUpZone,Direction.Down},
     };
     
     public WallMasterGrabScene(string currentRoomSprite, IDungeonRoom startRoom, ICommand loadRoomCommand, GraphicsDevice graphicsDevice)
@@ -65,14 +69,20 @@ public class WallMasterGrabScene : Scene
 
     private void UpdateDirection()
     {
-        foreach (var movementZone in DirectionMap.Keys)
+        // initialize priority value; first iteration should set actual priority value for comparison
+        int priority = 100;
+
+        // iteration over all zones and find which zone player is in
+        foreach (var testZone in DirectionMap.Keys)
         {
-            if (movementZone.Intersects(WallMasterRectangle))
+            Rectangle zoneRectangle = testZone.Item1;
+            if (zoneRectangle.Intersects(WallMasterRectangle))
             {
-                if (movementZone.Contains(WallMasterRectangle))
+                int zonePriority = testZone.Item2;
+                if (zonePriority < priority)
                 {
-                    movementDirection = DirectionMap[movementZone];
-                    return;
+                    priority = zonePriority;
+                    movementDirection = DirectionMap[testZone];
                 }
             }
         }
@@ -85,10 +95,11 @@ public class WallMasterGrabScene : Scene
         var position = PlayerState.Position;
         var curtainSize = new Point(512, 704);
         var Center = new Point(graphicsDevice.Viewport.Width / 2, 192);
-        LeftCurtain = new BlankSprite(SpriteLayer.HUD + 1, Center, curtainSize, Color.Black);
+        var leftPosition = Center - new Point(graphicsDevice.Viewport.Width / 2, 0);
+        LeftCurtain = new BlankSprite(SpriteLayer.HUD + 1, leftPosition, curtainSize, Color.Black);
         RightCurtain = new BlankSprite(SpriteLayer.HUD + 1, Center, curtainSize, Color.Black);
-        LeftCurtain.Enabled = true;
-        RightCurtain.Enabled = true;
+        LeftCurtain.Enabled = false;
+        RightCurtain.Enabled = false;
 
         // create fake background, link ,and wallmaster
         FakeWallMaster = new SpriteDict(SpriteType.Enemies, SpriteLayer.HUD, position);
@@ -100,11 +111,15 @@ public class WallMasterGrabScene : Scene
 
         // create FakeWallMaster rectangle
         WallMasterRectangle = new Rectangle(new Point(position.X - 32, position.Y - 32), new Point(52, 52));
+
+        // Create a 1x1 white texture for rectangle drawing
+        rectangleTexture = new Texture2D(graphicsDevice, 1, 1);
+        rectangleTexture.SetData(new[] { Color.White });
     }
 
     public override void Update(GameTime gameTime)
     {
-        if(StopZone.Contains(WallMasterRectangle) == false)
+        if(StopZone.Item1.Contains(WallMasterRectangle) == false)
         {
             UpdateDirection();
             Vector2 subtractionVector = 4 * DungeonConstants.DirectionVector[movementDirection];
@@ -114,20 +129,20 @@ public class WallMasterGrabScene : Scene
         }
         else
         {
+            FakeLink.Unregister();
+            FakeWallMaster.Unregister();
             LeftCurtain.Enabled = true;
             RightCurtain.Enabled = true;
             FakeBackground.SetSprite(startRoom.RoomSprite.ToString());
             CreateFakeDoors(startRoom);
-            if (LeftCurtain.Position.X != 0)
+            if (RightCurtain.Position.X != graphicsDevice.Viewport.Width)
             {
-                LeftCurtain.Position += new Point(4, 0);
-                RightCurtain.Position += new Point(-4, 0);
+                LeftCurtain.Position += new Point(-4, 0);
+                RightCurtain.Position += new Point(4, 0);
             }
             else
             {
                 FakeBackground.Unregister();
-                FakeLink.Unregister();
-                FakeWallMaster.Unregister();
                 PlayerState.Position = new Point(500, 725);
                 loadRoomCommand.Execute(startRoom.RoomName);
             }
